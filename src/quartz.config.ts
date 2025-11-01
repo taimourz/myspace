fbe743a09caeb378aeeb0da86a5d916aed4720b5
taimourz <taimour.rkt@gmail.com>
2025-11-02 01:35:23 +0500

    myspace to portfolio

--- src/quartz.config.ts
+++ src/quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "My Space",
+    pageTitle: "Portfolio",
     enableSPA: false,
     enablePopovers: true,
     analytics: {
